af09727bb3fffc89e5974fffb1ba8d91b2b9f0c2
Unknown <aps98@mail.ru>
2018-05-21 00:19:23 +0300

    Minor optimisation

--- plotGraphs.m
+++ plotGraphs.m
@@ -14,33 +14,33 @@ DATE=datetime;
 DATE(1)=[];
 
 b=1;
-    if isempty(varargin)
+    if isempty(varargin) %if theres no angle range input, then do for all angles
           for i=1:height(data_set)
                if strcmp(data_set{i,1},FrRange)
 
                             if strcmp(data_set{i,8},area) && (data_set{i,6}==0)
-                                  NUM(b)=data_set{i,2};
-                                  THETA(b)=data_set{i,3};
-                                  RCDB(b)=data_set{i,5};
-                                  DATE(b)=data_set{i,7};
-                                  b=b+1;
+                                  NUM(b)=data_set{i,2};     %create vector of numbers
+                                  THETA(b)=data_set{i,3};   %create vector of angles
+                                  RCDB(b)=data_set{i,5};    %create vector of values
+                                  DATE(b)=data_set{i,7};    %create vector of dates
+                                  b=b+1;                    %counter
                             end
 
                end
           end
+          %cut the empty parts
           NUM=NUM(1:b-1);
           THETA=THETA(1:b-1);
           RCDB=RCDB(1:b-1);
           DATE=DATE';
+          %create string, according to
          varname=strcat(strcat(area,'_'),FrRange);
-              description=varname;
-              eval(strcat(varname,'=table(NUM,THETA,RCDB,DATE);'));
-              eval(strcat('Out=',varname));
-              Out.Properties.Description=description;
+              Out=table(NUM,THETA,RCDB,DATE);
+              Out.Properties.Description=varname;
               Out.Properties.VariableNames={'Num','Theta','Rc_Db','Date'};
-    else
+    else %if there is angle range input, then do for given angles
             for i=1:height(data_set)
-               if strcmp(data_set{i,1},FrRange)
+               if strcmp(data_set{i,1},FrRange) 
 
                             if strcmp(data_set{i,8},area) && (data_set{i,6}==0) && (abs(data_set{i,3})>=varargin{1}) && (abs(data_set{i,3})<=varargin{2})
                                   NUM(b)=data_set{i,2};
@@ -57,10 +57,8 @@ b=1;
               RCDB=RCDB(1:b-1);
               DATE=DATE';
               varname=strcat(strcat(area,'_'),FrRange);
-              description=varname;
-              eval(strcat(varname,'=table(NUM,THETA,RCDB,DATE);'));
-              eval(strcat('Out=',varname));
-              Out.Properties.Description=description;
+              Out=table(NUM,THETA,RCDB,DATE);
+              Out.Properties.Description=varname;
               Out.Properties.VariableNames={'Num','Theta','Rc_Db','Date'};
               
               
