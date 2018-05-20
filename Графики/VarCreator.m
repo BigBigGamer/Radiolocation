46177c039d9afa329a4a9f57601129b68b8a8935
Unknown <ponur0kirill@gmail.com>
2018-05-20 23:53:20 +0300

    Ну шо погнали
    Добавлены графики и необходимые переменные

---
+++ Графики/VarCreator.m
@@ -0,0 +1,21 @@
+ from=3;
+ to=5;
+  [pine_Ka]=plotGraphs(alldata1,'pine','Ka',from,to);
+  [pine_Ku]=plotGraphs(alldata1,'pine','Ku',from,to);
+  [leaf_Ka]=plotGraphs(alldata1,'leaf','Ka',from,to);
+  [leaf_Ku]=plotGraphs(alldata1,'leaf','Ku',from,to);
+  [veld_Ka]=plotGraphs(alldata1,'veld','Ka',from,to);
+  [veld_Ku]=plotGraphs(alldata1,'veld','Ku',from,to);
+  
+  subplot(3,2,1)
+  plot(veld_Ku.Date,veld_Ku.Rc_Db,'*')
+  subplot(3,2,2)
+  plot(veld_Ka.Date,veld_Ka.Rc_Db,'*')
+  subplot(3,2,3)
+  plot(pine_Ku.Date,pine_Ku.Rc_Db,'*')
+  subplot(3,2,4)
+  plot(pine_Ka.Date,pine_Ka.Rc_Db,'*')
+  subplot(3,2,5)
+  plot(leaf_Ku.Date,leaf_Ku.Rc_Db,'*')
+  subplot(3,2,6)
+  plot(leaf_Ka.Date,leaf_Ka.Rc_Db,'*')
