ed1861ddd75f069ea5e460347c80e3b3d0f48378
Unknown <ponur0kirill@gmail.com>
2018-05-21 09:39:09 +0300

    Построены графики для Ku и Ka диапазона

--- Графики/Untitled.m
+++ Графики/Untitled.m
@@ -1,5 +1,3 @@
-figure
-title('3-5 degrees')
 hold on
 grid on
 grid minor
@@ -9,18 +7,42 @@ set(0,'defaultTextInterpreter','latex');
 set(0,'DefaultAxesFontSize',12);
 set(0,'DefaultTextFontSize',12);
 
-LEAF=f_leaf_Ka.pp1;
-LEAF1=f_leaf_Ka.pp2;
+LEAF=f_leaf_Ku.pp1;
+LEAF1=f_leaf_Ku.pp2;
+PINE=f_pine_Ku.pp1;
+PINE1=f_pine_Ku.pp2;
 
-PINE=f_pine_Ka.pp1;
-PINE1=f_pine_Ka.pp2;
 
-VELD=f_veld_Ka.pp1;
-VELD1=f_veld_Ka.pp2;
+VELD=f_veld_Ku.pp1;
+VELD1=f_veld_Ku.pp2;
+figure 
 
 plot(LEAF.XData,LEAF.YData,'r')
+hold on
+grid on
+grid minor
+plot(LEAF1.XData,LEAF1.YData,'.r')
+legend('leaf Ku fitted','leaf Ku dots')
+xlabel('time, days');
+ylabel('rcs, Db');
+figure 
 plot(PINE.XData,PINE.YData,'b')
+
+hold on
+grid on
+grid minor
+xlabel('time, days');
+ylabel('rcs, Db');
+plot(PINE1.XData,PINE1.YData,'.b')
+legend('pine Ku fitted','pine Ku dots')
+figure 
 plot(VELD.XData,VELD.YData,'m')
-% plot(LEAF1.XData,LEAF1.YData,'.r')
-% plot(PINE1.XData,PINE1.YData,'.b')
+hold on
+grid on
+grid minor
+xlabel('time, days');
+ylabel('rcs, Db');
+
+plot(VELD1.XData,VELD1.YData,'.m')
+legend('veld Ku fitted','veld Ku dots')
 legend('leaf Ka fitted','pine Ka fitted','veld Ka fitted')
--- Графики/Untitled2.m
+++ Графики/Untitled2.m
@@ -1,4 +1,4 @@
-table=pine_Ka;
+table=veld_Ku;
 table_name=table.Properties.Description;
 NumDate=datenum(table.Date);
 cftool(NumDate,table.Rc_Db);
